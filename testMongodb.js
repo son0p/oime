5c6434928600f96a260c2e11338fc955ce44c43d
fede2001 <fede2001@hotmail.com>
2013-10-01 18:33:10 -0500

    working exec child .. to exec some command and log stdout

--- testMongodb.js
+++ testMongodb.js
@@ -8,6 +8,20 @@
 * More here: http://mongodb.github.io/node-mongodb-native/markdown-docs/insert.html
 */
 
+var exec = require('child_process').exec,
+    child;
+child = exec('cat dataTestJson.txt',
+  function (error, stdout, stderr) {
+      console.log(stdout, stderr);      
+     
+       if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
+
+
+
+/*
 //require node modules (see package.json)
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
@@ -17,12 +31,24 @@ MongoClient.connect('mongodb://127.0.0.1:27017/mydb', function(err, db) {
   if (err) throw err;
   console.log("Connected to Database");
 
-  //simple json record
-	var document = {name:"David", title:"About MongoDB"};
   
+
+//simple json record
+   
+
+ var document = 	(child = exec('./phantomjs followersTest.js',
+    function (error, stdout, stderr) {
+        console.log(stdout, stderr);
+	var result = JSON.parse(stdout);
+    }
+));
 	//insert record
 	db.collection('testData').insert(document, function(err, records) {
 		if (err) throw err;
 		console.log("Record added as "+records[0]._id);
 	});
 });
+
+
+
+*/

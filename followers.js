145e078f08e00eefabd6ad46160dcec58e6aba30
fede2001 <fede2001@hotmail.com>
2013-10-29 09:36:30 -0500

    merge followers.js

---
+++ followers.js
@@ -0,0 +1,121 @@
+// List following and followers from several accounts
+
+var users = ['debrucesami',
+	     'providenciacol',
+	     'fede2001',
+	     'reygordiflon',
+	     'thecolombians',
+	     'elmismosebas',
+	     'unloquer',
+	     'estebangiraldo',
+	     'tarmacreggae',
+	     '_Donkristobal_',
+	     'selovendoco',
+	     'pulpomancito',
+	     'saleconelsol',
+	     'aerolineaada',
+	     'promimascota',
+	     'lionreggaemusic',
+	     'mrbleat',
+	     'panorama_laguna',
+	     'lscfj',
+	     'fonseca',
+	     'clopezcorrea',
+	     'viejasenbola',
+	     'juanes',
+	     'shakira',
+	     'MerlotteJRock',
+	     '_Volatil_',
+	     'Le_Mua',
+	     'ciudadpasarela',
+	     
+	    
+	    ];
+  var values = [];
+//var scoreArray = [];
+
+function follow(user, callback) {
+    var page = require('webpage').create();
+    page.open('https://twitter.com/' + user, function (status) {
+        if (status === 'fail') {
+            console.log(user + ': ?');
+        } else {
+            var data = page.evaluate(function () {
+                return document.querySelector('div.default-footer ul.stats.js-mini-profile-stats li a.js-nav[data-element-term=follower_stats] strong').innerText
+            });
+	    //console.log(  Date.now() + ',' + + ',' + user);
+
+	    values[values.length] = parseFloat(data.replace(/,/g, ''));
+
+	}
+
+	page.close();
+
+        callback.apply();
+
+    });
+
+}
+
+
+
+function process() {
+    if (users.length > 0) {
+        var user = users[0];
+        users.splice(0, 1);
+        follow(user, process);
+
+    } else {
+	console.log(Date.now() + "," + values);
+        phantom.exit();
+    }
+}
+
+function followingList() {
+    if (users.length > 0) {
+        var user = users[0];
+        users.splice(0, 1);
+        follow(user, process);
+
+    } else {
+        phantom.exit();
+    }
+}
+
+process();
+
+<<<<<<< HEAD
+// Los resultados se escriben a través de un servicio a la base de datos (o esa es la idea)
+// Example using HTTP POST operation
+/*
+var page = require('webpage').create(),
+    server = 'http://posttestserver.com/post.php?dump',
+    data = 'universe=expanding&answer=42';
+
+page.open(server, 'post', data, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to post!');
+    } else {
+        console.log(page.content);
+    }
+    phantom.exit();
+});
+
+*/
+=======
+// // Los resultados se escriben a través de un servicio a la base de datos (o esa es la idea)
+// // Example using HTTP POST operation
+
+// var page = require('webpage').create(),
+//     server = 'http://posttestserver.com/post.php?dump',
+//     data = 'universe=expanding&answer=42';
+
+// page.open(server, 'post', data, function (status) {
+//     if (status !== 'success') {
+//         console.log('Unable to post!');
+//     } else {
+//         console.log(page.content);
+//     }
+//     phantom.exit();
+// });
+>>>>>>> e7018d552dd886519ebbd75e660fbabadb6dd56d
